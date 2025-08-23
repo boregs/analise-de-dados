62dd9acef549a80e065461d5ad390a1b9d5c344e
Rafa <rafaborgesdasilva75@gmail.com>
2025-08-23 18:09:31 -0300

    Adicionados prompts de visualização de insights de dados

--- data_science/cleandata.py
+++ data_science/cleandata.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as mtl
+import matplotlib.pyplot as plt
 
 #projeto que pega dados de um banco de dados existente
 #limpa os dados para que sobre só os necessarios
@@ -34,7 +34,45 @@ df_vendas['valor_unitario'] = df_vendas['valor_unitario'].astype(float)
 
 # remove duplicatas
 df_vendas = df_vendas.drop_duplicates()
+df_vendas.to_csv('vendas_produtos_limpo.csv', index=False)
 
 # ---------------- VISUALIZANDO OS DADOS -----------------------
 
+#df_vendas_limpo["metodo_pagamento"].value_counts().plot(kind='bar', title='Metodos de pagamento mais usados') pandas tambem faz graficos, isso é um exemplo
+
+df_limpo = pd.read_csv(r'data_science\database\vendas_produtos_limpo.csv')
+
+# rrotudos mais comprados
+def tendencia_produtos_comprados():
+    vendas_por_produto = df_vendas.groupby('nome_produto')['quantidade'].sum() # agrupa os produtos com suas respectivas quantidades de vendas dentro de toda database
+    vendas_por_produto = vendas_por_produto.sort_values(ascending=False) # arruma em ordem decrescente
+
+    produtos = vendas_por_produto.index # atribui os nomes dos produtos a variavel produto
+    quantidades = vendas_por_produto.values # atribui a quantidade de venda total a variavel quantidade
+
+    # o basico para criar um grafico de barras simples
+    plt.figure(figsize=(8,5)) # ajeita o tamanho da figura
+    plt.bar(produtos, quantidades, color='lightblue')
+    plt.xlabel("Produtos")
+    plt.ylabel("Quantidade de vendas")
+    plt.title("Produtos mais comprados no mês de Julho")
+    plt.xticks(produtos, rotation=45, fontsize=8) # rotaciona os nomes dos produtos no grafico e altera a fonte deles
+    plt.show()
+
+# metodo de pagamento mais usado
+def metodo_pagamento():
+    formas_pagamento = df_vendas.groupby('metodo_pagamento')['quantidade'].sum() # agrupa os produtos com suas respectivas quantidades de vendas dentro de toda database
+    formas_pagamento = formas_pagamento.sort_values(ascending=False) # arruma em ordem decrescente
+
+    forma = formas_pagamento.index # atribui os nomes dos produtos a variavel produto
+    quantidade = formas_pagamento.values # atribui a quantidade de venda total a variavel quantidade
+
+    # o basico para criar um grafico de barras simples
+    plt.figure(figsize=(8,5)) # ajeita o tamanho da figura
+    plt.bar(forma, quantidade, color='lightblue')
+    plt.xlabel("Produtos")
+    plt.ylabel("Quantidade de vendas")
+    plt.title("Produtos mais comprados no mês de Julho")
+    plt.xticks(forma, rotation=45, fontsize=8) # rotaciona os nomes dos produtos no grafico e altera a fonte deles
+    plt.show()
 
